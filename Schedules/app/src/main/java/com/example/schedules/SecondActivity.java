a8eb11d00c956a0d956e3d0d3b2999ffa564014e
RedJanvier <janvierntwali@gmail.com>
2021-11-16 10:50:06 +0200

    feat(secondActivity): create and link

---
+++ Schedules/app/src/main/java/com/example/schedules/SecondActivity.java
@@ -0,0 +1,47 @@
+package com.example.schedules;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        // calling the action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("Second Activity");
+        // showing the back button in action bar
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        Button openMain = (Button) findViewById(R.id.open_main_button);
+        openMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SecondActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    // this event will enable the back
+    // function to the button on press
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
